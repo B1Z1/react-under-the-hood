ab27474e28c7abbfa161ac7152803b48aecf0416
B1Z1 <iluhamishkin@gmail.com>
2025-05-29 16:16:00 +0200

    Create a simple rerender for my small react application

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 let React = {
     createElement: (tag, props, ...children) => {
         if (typeof tag === 'function') {
-            return tag(props);
+            try {
+                return tag(props);
+            } catch ({ promise, key }) {
+                promise.then(data => {
+                    promiseCache.set(key, data);
+                    rerender();
+                })
+                return { tag: 'div', props: { children: ["I'm loading"] } };
+            }
         }
 
         const element = {
@@ -15,18 +23,71 @@ let React = {
     }
 };
 
-const App = () => (
-    <div className="work">
-        <h2>
-            Hello, person
-        </h2>
-        <p>
-            This is a simple React component that demonstrates how to use JSX syntax.
-            The component renders a div with a class name of "work", containing a heading and a paragraph.
-            You can modify the content as needed to fit your application.
-        </p>
-    </div>
-);
+const states = [];
+let stateCursor = 0;
+
+const useState = (initialState) => {
+    const FROZEN_CURSOR = stateCursor;
+
+    states[FROZEN_CURSOR] = states[FROZEN_CURSOR] ?? initialState;
+
+    let setState = (newState) => {
+        states[FROZEN_CURSOR] = newState;
+        rerender();
+    };
+
+    stateCursor++;
+
+    return [states[FROZEN_CURSOR], setState];
+}
+
+const promiseCache = new Map();
+
+const createResource = (promise: Promise<any>, key: string) => {
+    if (promiseCache.has(key)) {
+        return promiseCache.get(key);
+    }
+
+    throw { promise, key };
+}
+
+const App = () => {
+    const [name, setName] = useState('World');
+    const [count, setCount] = useState(0);
+    const dogPhotoUrl = createResource(
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(payload => payload.message),
+        "dogPhoto"
+    );
+
+    return (
+        <div className="work">
+            <h2>
+                Hello, {name}
+            </h2>
+            <input
+                className="input"
+                type="text"
+                value={name}
+                onchange={(e) => setName(e.target.value)}
+                placeholder="Type something here..."
+            />
+
+            <h2>The count is: {count}</h2>
+            <button onclick={() => setCount(count + 1)}>Increase</button>
+            <button onclick={() => setCount(count - 1)}>Decrease</button>
+
+            <img src={dogPhotoUrl} alt="Work"/>
+
+            <p>
+                This is a simple React component that demonstrates how to use JSX syntax.
+                The component renders a div with a class name of "work", containing a heading and a paragraph.
+                You can modify the content as needed to fit your application.
+            </p>
+        </div>
+    );
+};
 
 const render = (reactElement, container: HTMLElement) => {
     if (["string", "number"].includes(typeof reactElement)) {
@@ -54,6 +115,14 @@ const render = (reactElement, container: HTMLElement) => {
     }
 
     container.appendChild(actualDomElement);
+}
+
+const rerender = () => {
+    stateCursor = 0;
+
+    document.getElementById('app').firstChild?.remove();
+
+    render(<App/>, document.getElementById('app'));
 }
 
 render(<App/>, document.getElementById('app'));
